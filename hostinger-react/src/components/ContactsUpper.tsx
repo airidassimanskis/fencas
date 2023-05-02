56dd6d62734a9bbccd84b42cc8417d0c0e78815b
Shimis <78961147+Shinigamiixd@users.noreply.github.com>
2023-05-02 09:41:19 +0300

    pridejau icons ir hrefus

--- hostinger-react/src/components/ContactsUpper.tsx
+++ hostinger-react/src/components/ContactsUpper.tsx
@@ -1,16 +1,36 @@
+import { PhoneIcon } from "@heroicons/react/24/outline"
+import { AtSymbolIcon } from "@heroicons/react/24/solid"
+
 const ContactsUpper = () => {
     return (
         <div>
-            <div>
-                email icon <b>info@fencas.lt</b>
-            </div>
-            <div>
-                tefono icon <b>+37060813610</b>
-            </div>
-
-            <div>
-                <b>+37063093384</b>
-            </div>
+            <li>
+                <div className="flex leading-4">
+                    <AtSymbolIcon className="h-5 w-6 text-black-500 mr-1" />
+                    <a
+                        className="hover:text-blue-700"
+                        href="mailto:info@fencas.lt"
+                    >
+                        info@fencas.lt
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div className="flex leading-4">
+                    <PhoneIcon className="h-5 w-6 text-black-500 mr-1" />
+                    <a className="hover:text-blue-700" href="tel:+37060813610">
+                        +37060813610
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div className="flex leading-4">
+                    <PhoneIcon className="h-5 w-6 text-black-500 mr-1" />
+                    <a className="hover:text-blue-700" href="tel:+37063093384">
+                        +37063093384
+                    </a>
+                </div>
+            </li>
         </div>
     )
 }
--- hostinger-react/src/components/Footer.tsx
+++ hostinger-react/src/components/Footer.tsx
@@ -1,41 +1,62 @@
-import { Typography } from "@material-tailwind/react";
- 
+import { Typography } from "@material-tailwind/react"
+import { PhoneIcon } from "@heroicons/react/24/outline"
+import { AtSymbolIcon } from "@heroicons/react/24/solid"
+
 export default function Footer() {
-  return (
-    <footer className="w-full bg-white p-8">
-      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
-        <img src="/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
-        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
-          <li>
-            <Typography
-              color="blue-gray"
-              className="font-normal"
-            >
-              info@fencas.lt
+    return (
+        <footer className="w-full bg-white p-8">
+            <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
+                <img
+                    src="/img/logo-ct-dark.png"
+                    alt="logo-ct"
+                    className="w-10"
+                />
+                <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+                    <li>
+                        <Typography color="blue-gray" className="font-normal">
+                            <div className="flex leading-4">
+                                <AtSymbolIcon className="h-5 w-6 text-black-500" />
+                                <a
+                                    className="hover:text-blue-700"
+                                    href="mailto:info@fencas.lt"
+                                >
+                                    info@fencas.lt
+                                </a>
+                            </div>
+                        </Typography>
+                    </li>
+                    <li>
+                        <Typography color="blue-gray" className="font-normal">
+                            <div className="flex leading-4">
+                                <PhoneIcon className="h-5 w-6 text-black-500" />
+                                <a
+                                    className="hover:text-blue-700"
+                                    href="tel:+37060813610"
+                                >
+                                    +37060813610
+                                </a>
+                            </div>
+                        </Typography>
+                    </li>
+                    <li>
+                        <Typography color="blue-gray" className="font-normal">
+                            <div className="flex leading-4">
+                                <PhoneIcon className="h-5 w-6 text-black-500" />
+                                <a
+                                    className="hover:text-blue-700"
+                                    href="tel:+37063093384"
+                                >
+                                    +37063093384
+                                </a>
+                            </div>
+                        </Typography>
+                    </li>
+                </ul>
+            </div>
+            <hr className="my-8 border-blue-gray-50" />
+            <Typography color="blue-gray" className="text-center font-normal">
+                &copy; 2023 fencas.lt
             </Typography>
-          </li>
-          <li>
-            <Typography
-              color="blue-gray"
-              className="font-normal"
-            >
-              +37060813610
-            </Typography>
-          </li>
-          <li>
-            <Typography
-              color="blue-gray"
-              className="font-normal"
-            >
-              +37063093384
-            </Typography>
-          </li>
-        </ul>
-      </div>
-      <hr className="my-8 border-blue-gray-50" />
-      <Typography color="blue-gray" className="text-center font-normal">
-        &copy; 2023 fencas.lt
-      </Typography>
-    </footer>
-  );
+        </footer>
+    )
 }
