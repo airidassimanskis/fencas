1ddcba2a1a94e7c92db9a4fc67d29b36ed09e9df
Shimis <78961147+Shinigamiixd@users.noreply.github.com>
2023-05-05 00:00:15 +0300

    sticky navbar on scroll

--- hostinger-react/src/components/Navbar.tsx
+++ hostinger-react/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import { Disclosure } from "@headlessui/react"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 
@@ -12,16 +13,38 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(" ")
 }
 
-export default function Example() {
+export default function Navbar() {
+    const [sticky, setSticky] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setSticky(window.pageYOffset > 144)
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure
+            as="nav"
+            className={`${
+                sticky ? "fixed top-0 w-full z-50" : ""
+            } bg-gray-800 transition-all duration-500 ease-in-out`}
+        >
             {({ open }) => (
-                <>
+                <div>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                        <div className="relative flex h-28 items-center justify-between">
+                        <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button
+                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                    aria-label="Toggle menu"
+                                >
                                     <span className="sr-only">
                                         Atskleisti meniu
                                     </span>
@@ -41,13 +64,13 @@ export default function Example() {
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
                                     <img
-                                        className="block h-24 w-auto md:hidden"
+                                        className="block h-14 w-auto md:hidden"
                                         src="./fencas_full.png"
                                         alt="Fencas"
                                         draggable="false"
                                     />
                                     <img
-                                        className="hidden h-24 w-auto md:block"
+                                        className="hidden h-14 w-auto md:block"
                                         src="./fencas.png"
                                         alt="Fencas"
                                         draggable="false"
@@ -79,30 +102,28 @@ export default function Example() {
                             </div>
                         </div>
                     </div>
-
                     <Disclosure.Panel className="sm:hidden">
-                        <div className="space-y-1 px-2 pb-3 pt-2">
+                        <div className="px-2 pt-2 pb-3 space-y-1">
                             {navigation.map((item) => (
-                                <Disclosure.Button
+                                <a
                                     key={item.name}
-                                    as="a"
                                     href={item.href}
                                     className={classNames(
                                         item.current
                                             ? "bg-gray-900 text-white"
                                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                                        "block rounded-md px-3 py-2 text-base font-medium"
+                                        "block px-3 py-2 rounded-md text-base font-medium"
                                     )}
                                     aria-current={
                                         item.current ? "page" : undefined
                                     }
                                 >
                                     {item.name}
-                                </Disclosure.Button>
+                                </a>
                             ))}
                         </div>
                     </Disclosure.Panel>
-                </>
+                </div>
             )}
         </Disclosure>
     )
